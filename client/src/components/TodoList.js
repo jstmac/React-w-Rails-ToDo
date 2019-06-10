8b5b8b4e125f1cc979ce4592ad4d6fbeb02d1219
Melissa G <jstmac@gmail.com>
2019-06-10 12:49:13 -0600

    Working

---
+++ client/src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = ({ todos, updateTodo, deleteTodo }) => (
+  <div>
+    {todos.map(todo => (
+      <Todo
+        key={todo.id}
+        {...todo}
+        updateTodo={updateTodo}
+        deleteTodo={deleteTodo}
+      />
+    ))}
+  </div>
+);
+
+export default TodoList;
